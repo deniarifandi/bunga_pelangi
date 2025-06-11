a68ab7b4ee3b9a68de13f3fba419e2707a5db643
deniarifandi <42629835+deniarifandi@users.noreply.github.com>
2025-06-12 06:01:42 +0700

    Update print_rapot.php

--- print_rapot.php
+++ print_rapot.php
@@ -430,7 +430,7 @@ echo $list_hasil_array[$i]['amati_6']."\n";
 						
 						<th class="borderless" style="width:5%"></th>
 
-						<th class="borderless" style="width: 25%; text-align: right;">Malang, <?php echo $tanggalrapot ?></th>
+						<th class="borderless" style="width: 25%; text-align: center;">Malang, <?php echo $tanggalrapot ?></th>
 
 						<th class="borderless" style="width:10%"></th>
 					</tr>
