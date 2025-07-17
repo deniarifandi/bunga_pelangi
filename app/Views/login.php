3888ae50af6216423ccb5fa1eed2b1f1e903649e
deniarifandi <42629835+deniarifandi@users.noreply.github.com>
2025-07-17 19:05:07 +0700

    Update login.php

--- app/Views/login.php
+++ app/Views/login.php
@@ -73,7 +73,9 @@
   <body class="login-page bg-body-secondary" style="display: flex; justify-content: center; align-items: center; height: 100vh;  ">
     <div class="login-box" style="width: 600px;">
       <div class="login-logo">
-        <b>My Little Island School Portal</b><br> <h5>SinaRumi v.3.0.0</h5>
+        <b style="font-size:20px">Sistem Informasi Sekolah</b> <br>
+          Pos Paud Bunga Pelangi
+        <br> <h5>SinaRumi v.3.0.0</h5>
       </div>
       <!-- /.login-logo -->
       <div class="card" style="width:360px; margin: 0 auto;">
